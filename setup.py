acfa878ce8dab0ae15af956393a76d5608eb42ba
Edward Lau <edward.lau@me.com>
2020-06-02 09:52:45 -0600

    Removing all files in .gitignore

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from jcast import __version__
+
+setup(
+   name='Jcast',
+   version=__version__,
+   description='Jcast retrieves splice junction information and translates into amino acid',
+   author='Edward Lau',
+   url='https://www.laulab.net',
+   author_email='edward.lau@cuanschutz.edu',
+   packages=['jcast'],  #same as name
+   install_requires=['biopython', 'gtfparse', 'pandas', 'requests', 'tqdm'], #external packages as dependencies
+)
