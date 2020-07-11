d44b261e358a6e7bbf2982b16349aa6358875db0
Edward Lau <edward.lau@me.com>
2020-07-11 13:55:36 -0600

    added rat gtf test

---
+++ tests/test_gtf.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+""" Tests """
+
+
+import unittest
+import tempfile
+import sys
+import os
+from jcast.get_gtf import ReadAnnotations
+
+
+class AnnotationTest(unittest.TestCase):
+    """
+    Test cases involving reading genome sequence and references
+    """
+
+    def setUp(self):
+        """
+
+        :return:
+        """
+
+        global test_data_loc, human_gtf_loc, rat_gtf_loc
+
+
+        test_data_loc = os.path.join('tests', 'data')
+        human_gtf_loc = os.path.join(test_data_loc, 'genome', 'Homo_sapiens.GRCh38.89.chromosome.15.gtf')
+        rat_gtf_loc = os.path.join(test_data_loc, 'genome', 'Rattus_norvegicus.Rnor_6.0.100.chromosome.12.gtf')
+
+        # Remove the cached gtf file
+        try:
+            os.remove(human_gtf_loc + '.cached')
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.remove(rat_gtf_loc + '.cached')
+        except FileNotFoundError:
+            pass
+
+    def tearDown(self):
+
+        # Remove the cached gtf file
+        try:
+            os.remove(human_gtf_loc + '.cached')
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.remove(rat_gtf_loc + '.cached')
+        except FileNotFoundError:
+            pass
+
+        pass
+
+    def test_human_tss(self):
+        """ finds the translation start of a human gene """
+
+        human_gtf = ReadAnnotations(human_gtf_loc)
+        human_gtf.read_gtf()
+
+        gtf0 = human_gtf.annot.query('gene_id == "ENSG00000104055"')
+        self.assertGreater(len(gtf0), 0)
+
+        # Get the coding sequence start codon locs, sort by transcript_support_level
+        gtf0_start = gtf0.query('feature == "start_codon"').sort_values(['transcript_support_level']).sort_values(
+            ['ccds_id']).loc[:, 'start']
+        self.assertGreater(len(gtf0_start), 0)
+
+        # Translation start site
+        tx0 = gtf0_start.iloc[0]
+        self.assertGreater(tx0, 0)
+
+        pass
+
+    def test_rat_tss(self):
+        """ finds the translation start of a rat gene """
+
+        rat_gtf = ReadAnnotations(rat_gtf_loc)
+        rat_gtf.read_gtf()
+
+        gtf0 = rat_gtf.annot.query('gene_id == "ENSRNOG00000055204"')
+        self.assertGreater(len(gtf0), 0)
+
+        gtf0_start = gtf0.query('feature == "start_codon"').loc[:, 'start']
+        self.assertGreater(len(gtf0_start), 0)
+        tx0 = gtf0_start.iloc[0]
+
+        # Translation start site
+        tx0 = gtf0_start.iloc[0]
+        self.assertGreater(tx0, 0)
+
+        pass
+
+
